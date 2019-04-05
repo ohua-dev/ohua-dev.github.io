31eaacef53e1e61ea6b41e40d1cc6231a88a9427
Justus Adam <dev@justus.science>
2019-04-05 17:51:02 +0200

    Corrected java code, changed header color

--- _includes/snippets/java/SimpleExample.java
+++ _includes/snippets/java/SimpleExample.java
@@ -1,6 +1,10 @@
 class State {
     private List<String> _alreadyGreeted = new LinkedList<>();
 
+    public static State create() {
+        return new State();
+    }
+
     String greetings(String name){
         String greeting = "Hello " + name + "\nI already greeted all these guys: " + _alreadyGreeted;
         _alreadyGreeted.add(name);
